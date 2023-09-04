98918c90c860409fe401cbbd9408e272de1976be
theisolatedmonk <96rahulseth@gmail.com>
2023-09-04 18:26:42 +0530

    form structure created

---
+++ app/components/Form.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Image from 'next/image'
+import alertIcon from '@/public/images/icon-error.svg'
+
+type FormType = {
+    alert: string
+    placeholder: string
+
+}
+
+export function FormElement(props: FormType) {
+    return (
+        <div className="flex flex-col text-[hsl(249,10%,26%)] gap-1">
+            <div className="flex border-gray-500 border p-2 rounded-lg justify-between">
+                <input type="text" placeholder={props.placeholder} className='w-full' />
+                <Image src={alertIcon} alt={''} />
+            </div>
+            <p className="text-[hsl(0,100%,74%)] text-right">{props.alert}</p>
+        </div>
+
+    )
+}
+
+
+type Props = {}
+
+export default function Form({ }: Props) {
+    return (
+        <div className='flex flex-col bg-white   rounded-lg p-4 gap-4'>
+            <FormElement alert={'First Name cannot be empty'} placeholder={'Name'} />
+            <FormElement alert={'Last Name cannot be empty'} placeholder={'Last Name'} />
+            <FormElement alert={'Look like this is not an email '} placeholder={'email@example/com'} />
+            <FormElement alert={'Password cannot be empty'} placeholder={'Password'} />
+
+            <button className='bg-[hsl(154,59%,51%)] p-2 rounded-md'>CLAIM YOUR FREE TRAIL</button>
+            <p className="text-[hsl(246,25%,77%)]">By clicking th e button, you are agreeing to our <span className='text-[hsl(0,100%,74%)]'>Terms and Services</span></p>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import Image from 'next/image'
+import backgrondDesktop from '@/public/images/bg-intro-desktop.png'
+import Form from './components/Form'
 
 export default function Home() {
   return (
-    <div className="flex w-full h-screen">
-
+    <div className="flex w-full h-screen bg-[hsl(0,100%,74%)]">
+      <div className="h-full flex gap-2">
+        <Image src={backgrondDesktop} alt={''} className='absolute h-full' />
+        <div className="flex flex-col">
+          <div className="">Learn to code by watching others</div>
+          <div className="">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</div>
+        </div>
+        <div className="flex flex-col">
+          <div className="">Try it free 7 days <span>than $20/mo. thereafter</span></div>
+          <Form></Form>
+        </div>
+      </div>
     </div>
   )
 }
